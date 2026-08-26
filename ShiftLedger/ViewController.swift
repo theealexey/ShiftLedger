4f94dfe5dca6a7cc689693d36825d98eba3d4022
theealexey <theealexey@gmail.com>
2026-08-26 22:50:19 +0300

    Configure programmatic UIKit entry point

--- ShiftLedger/ViewController.swift
+++ ShiftLedger/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  ShiftLedger
-//
-//  Created by Alexey Westergaard on 26.08.2026.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
     }
-
-
 }
-
