6cfb1003126395d873f92e62d9b70736d45228cb
theealexey <theealexey@gmail.com>
2026-08-26 22:32:25 +0300

    Initial Commit

---
+++ ShiftLedgerTests/ShiftLedgerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftLedgerTests.swift
+//  ShiftLedgerTests
+//
+//  Created by Alexey Westergaard on 26.08.2026.
+//
+
+import Testing
+@testable import ShiftLedger
+
+struct ShiftLedgerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
