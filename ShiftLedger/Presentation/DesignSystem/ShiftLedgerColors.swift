e02112b01a894432e08c0d5cfbc7b61684fe5ce5
theealexey <theealexey@gmail.com>
2026-08-29 15:49:34 +0300

    Add visual design tokens

---
+++ ShiftLedger/Presentation/DesignSystem/ShiftLedgerColors.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+enum ShiftLedgerColors {
+    static let backgroundPrimary = UIColor(resource: .DesignSystem.backgroundPrimary)
+    static let backgroundSecondary = UIColor(resource: .DesignSystem.backgroundSecondary)
+    static let surfacePrimary = UIColor(resource: .DesignSystem.surfacePrimary)
+
+    static let textPrimary = UIColor(resource: .DesignSystem.textPrimary)
+    static let textSecondary = UIColor(resource: .DesignSystem.textSecondary)
+    static let textTertiary = UIColor(resource: .DesignSystem.textTertiary)
+
+    static let accentPrimary = UIColor(resource: .DesignSystem.accentPrimary)
+    static let separator = UIColor(resource: .DesignSystem.separator)
+
+    static let statusPositive = UIColor(resource: .DesignSystem.statusPositive)
+    static let statusNegative = UIColor(resource: .DesignSystem.statusNegative)
+    static let statusWarning = UIColor(resource: .DesignSystem.statusWarning)
+}
---
+++ ShiftLedger/Presentation/DesignSystem/ShiftLedgerTypography.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+enum ShiftLedgerTypography {
+    static var display: UIFont {
+        let font = UIFont.monospacedDigitSystemFont(ofSize: 40, weight: .bold)
+        return UIFontMetrics(forTextStyle: .largeTitle).scaledFont(for: font)
+    }
+
+    static var largeTitle: UIFont {
+        UIFont.preferredFont(forTextStyle: .largeTitle)
+    }
+
+    static var title: UIFont {
+        UIFont.preferredFont(forTextStyle: .title1)
+    }
+
+    static var headline: UIFont {
+        UIFont.preferredFont(forTextStyle: .headline)
+    }
+
+    static var body: UIFont {
+        UIFont.preferredFont(forTextStyle: .body)
+    }
+
+    static var callout: UIFont {
+        UIFont.preferredFont(forTextStyle: .callout)
+    }
+
+    static var caption: UIFont {
+        UIFont.preferredFont(forTextStyle: .caption1)
+    }
+
+    static var button: UIFont {
+        UIFont.preferredFont(forTextStyle: .headline)
+    }
+}
